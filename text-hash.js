ac43b23757e570aee4961e945db00889a82434b6
BeTrumBao <betrumbaodeptraivippro@gmail.com>
2025-02-07 21:43:03 +0700

    Create text-hash.js

---
+++ text-hash.js
@@ -0,0 +1,10 @@
+{
+  "line1": {
+    "text": "Đây là dòng chữ đầu tiên",
+    "hash": "d1e05e07c5377d432d416b897c9a9351b6e12627e139c91988cfd0eec29e35fc"
+  },
+  "line2": {
+    "text": "Đây là dòng chữ thứ hai",
+    "hash": "3b2a6a3b3e64d9e917c05194e2d5e263ff7c482f9995b813d169160d971436bc"
+  }
+}
