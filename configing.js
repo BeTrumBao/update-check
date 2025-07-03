ffb4eaacbd9b2769cd8760e24e2a7581fa5d47b7
BeTrumBao <betrumbaodeptraivippro@gmail.com>
2025-07-03 20:22:18 +0700

    Update configing.js

--- configing.js
+++ configing.js
@@ -11,7 +11,7 @@
     "maintenance_message": "Hệ thống đang bảo trì để chuẩn bị cho sự kiện lớn. Vui lòng quay lại sau!",
     "enable_weekend_challenge": false,
     "enable_ranking_matches": false,
-    "rank_maintenance_mode": false,
+    "rank_maintenance_mode": true,
     "rank_maintenance_message": "Hệ thống xếp hạng đang được bảo trì để chuẩn bị cho mùa giải mới. Bạn có thể quay lại sau."
   },
   "app_config": {
