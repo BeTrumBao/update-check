7488697277ac2a002e70f0300f8c165aaa3c4303
BeTrumBao <betrumbaodeptraivippro@gmail.com>
2025-07-04 21:13:10 +0700

    Create newsing.js

---
+++ newsing.js
@@ -0,0 +1,16 @@
+[
+  {
+    "id": "patch_3.6",
+    "title": "Cập nhật v3.6: Chế độ Sự kiện!",
+    "publish_date": "2025-07-04T22:00:00+07:00",
+    "author": "Bảo Neyako",
+    "content": "### Chào mừng các Lập trình viên!\n\nĐây là bản cập nhật lớn đầu tiên, mang đến **Hệ thống Sự kiện** hoàn toàn mới để giữ cho ứng dụng luôn mới mẻ.\n\n#### Các thay đổi chính:\n* Thêm cửa sổ quản lý sự kiện.\n* Nâng cấp giao diện Bảng Xếp Hạng.\n* Sửa một số lỗi về hiển thị điểm số và thứ hạng.\n\nCảm ơn sự ủng hộ của các bạn!"
+  },
+  {
+    "id": "welcome_new_season",
+    "title": "Mùa giải mới: Kỷ Nguyên Công Nghệ đã bắt đầu!",
+    "publish_date": "2025-07-01T00:00:00+07:00",
+    "author": "Hệ thống",
+    "content": "Mùa giải mới đã chính thức khởi tranh. Điểm xếp hạng đã được reset.\n\n**Phần thưởng đặc biệt** sẽ được trao cho top 3 vào cuối mùa. Chúc các bạn may mắn!"
+  }
+]
