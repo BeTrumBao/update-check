3f58bc3d1d1b4b5725018383af08865db5bbcfa1
BeTrumBao <betrumbaodeptraivippro@gmail.com>
2025-07-04 20:03:17 +0700

    Create eventing.js

---
+++ eventing.js
@@ -0,0 +1,22 @@
+[
+  {
+    "id": "x2_weekend_jul_1",
+    "title": "Sự Kiện x2 Điểm Cuối Tuần",
+    "description": "Nhân đôi số điểm nhận được từ tất cả các thử thách xếp hạng trong thời gian diễn ra sự kiện. Hãy tranh thủ leo hạng!",
+    "type": "xp_multiplier",
+    "value": 2,
+    "start_time": "2025-07-05T00:00:00+07:00",
+    "end_time": "2025-07-06T23:59:59+07:00",
+    "icon": "🎉"
+  },
+  {
+    "id": "ultimate_practice_jul",
+    "title": "Tuần Lễ Thử Thách Tối Thượng",
+    "description": "Trong tuần này, tất cả người chơi đều có thể tham gia 'Cuộc Chiến Tối Thượng' bất kể thứ hạng. Phần thưởng điểm sẽ được giảm một nửa.",
+    "type": "unlock_feature",
+    "value": "ultimate_battle",
+    "start_time": "2025-07-07T00:00:00+07:00",
+    "end_time": "2025-07-13T23:59:59+07:00",
+    "icon": "⚔️"
+  }
+]
